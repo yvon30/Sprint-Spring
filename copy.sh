3e98d17cc3afa47874dc8d9b9c4349267a270416
yvon30 <tantely.yvon03@gmail.com>
2024-06-25 07:02:20 +0300

    Mety
    Mety

--- copy.sh
+++ copy.sh
@@ -1,2 +1,2 @@
-cp sprint_etu002713.jar ../usage_frameworks/lib/
-cp sprint_etu002713.jar ../usage_frameworks/WEB/WEB-INF/lib/
+cp sprint_2739.jar ../usage_frameworks/lib/
+cp sprint_2739.jar ../usage_frameworks/WEB/WEB-INF/lib/
--- src/util/ModelView.java
+++ src/util/ModelView.java
@@ -3,40 +3,41 @@ package util;
 import java.util.HashMap;
 
 public class ModelView {
-    HashMap<String,Object> data;
+    HashMap<String, Object> data;
     String name;
 
-    public ModelView(){
+    public ModelView() {
         data = new HashMap<>();
     }
-    public ModelView(String name){
+
+    public ModelView(String name) {
         this();
         this.name = name;
     }
-    
-    public void setName(String name){
+
+    public void setName(String name) {
         this.name = name;
     }
-    
-    public String getName(){
+
+    public String getName() {
         return this.name;
     }
+
     /**
      * @return the data
      */
     public HashMap<String, Object> getData() {
         return data;
     }
+
     /**
      * @param data the data to set
      */
     public void setData(HashMap<String, Object> data) {
         this.data = data;
     }
-    
-    
 
-    public void addObject(String object_name,Object object){
+    public void addObject(String object_name, Object object) {
         data.put(object_name, object);
     }
 }
